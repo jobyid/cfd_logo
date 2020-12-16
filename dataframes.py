9d15000821322bf94a7c769438b0135cd94fd5be
Joby Ingram-Dodd <joby@protonmail.com>
2020-12-16 20:54:29 +0000

    Dataframe Progress

--- dataframes.py
+++ dataframes.py
@@ -3,6 +3,7 @@ import numpy as np
 import moonFunctions as mf
 from datetime import datetime
 
+# Moon data frame
 md2 = pd.read_csv("Moon phase on 3 January 1984P2.csv")
 md1 = pd.read_csv("Moon phase on 3 January 1984.csv")
 m_df  = pd.DataFrame(md1)
@@ -14,10 +15,25 @@ m_df['date'] = m_df['date'].map(lambda x: x.lstrip('Moon phase details at '))
 m_df['date'] = m_df['date'].map(lambda x:datetime.strptime(x, "%d %B %Y"))
 m_df['dist_to_moon'] = m_df['dist_to_moon'].map(lambda x: x.rstrip('km').replace(",",""))
 m_df['illumination'] = m_df['illumination'].map(lambda x: x.rstrip('% Visible'))
+m_df.sort_values(by=['date'], inplace=True)
 
-fd = pd.read_csv("^FTSE.csv")
+# FTSE Data Frame
+fd = pd.read_csv("FTSE 100 - 1984-2020.csv")
 f_df = pd.DataFrame(fd)
-f_df.rename(columns = {'Date':'date'}, inplace = True)
-f_df['date'] = f_df['date'].map(lambda x:datetime.strptime(x, "%Y-%m-%d"))
+f_df.rename(columns = {'Date':'date','Open':'open_d-1', 'High':'high_d-1','Low':'low_d-1', 'Close_':'close_d-1'}, inplace = True)
+f_df['date'] = pd.to_datetime(f_df['date'], format="%b %d, %Y")
+f_df['weekday'] = f_df['date'].dt.dayofweek
+f_df['month'] = f_df['date'].dt.month
+f_df['open_d-1'] = f_df['open_d-1'].shift(-1)
+f_df['high_d-1'] = f_df['high_d-1'].shift(-1)
+f_df['low_d-1'] = f_df['low_d-1'].shift(-1)
+f_df['close_d-1'] = f_df['close_d-1'].shift(-1)
+# Adjusted Close is the result, d-1 columns are the day before figures
+# NOTE: .shift() to move columns up or down.
+
+
+# merged data frame
 final = pd.merge(m_df, f_df, on='date')
-print(m_df.size)
+print(f_df.tail())
+##print(m_df.tail())
+#print(final.tail())
